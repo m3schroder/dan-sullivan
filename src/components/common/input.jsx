2359ec87fe87990530dd45f16fa74e4168b16c55
m3schroder <m3schroder@gmail.com>
2021-07-18 12:56:18 -0600

    either

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 
 const Input = ({ name, label, required, error, errDropdown, ...rest }) => {
-  let inputClass = "form-control";
-  if (errDropdown) {
-    console.log("Working");
-    if (!error) {
-      inputClass = "";
-    } else {
-      inputClass += "alert-danger";
-      error = null;
-    }
+  let inputClass = "";
+  if (errDropdown && error) {
+    inputClass = "alert-danger";
+    error = null;
   }
   return (
     <div className="form-group">
@@ -21,7 +16,7 @@ const Input = ({ name, label, required, error, errDropdown, ...rest }) => {
         {...rest}
         name={name}
         id={name}
-        className={inputClass}
+        className={"form-control " + inputClass}
         placeholder={label}
       ></input>
       {error && <div className="alert-danger form-error">{error}</div>}
